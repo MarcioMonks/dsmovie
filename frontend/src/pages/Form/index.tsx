868ad5b0de46db78b207cceaf80cf39ca7d31f23
Márcio Monks <marcio.monks@gmail.com>
2022-03-07 14:03:58 -0300

    Navigations buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 function Form() {
@@ -40,7 +41,12 @@ function Form() {
             </button>
           </div>
         </form>
-        <button className="btn btn-primary mymovies-btn mt-3">Cancelar</button>
+
+        <Link to="/">
+          <button className="btn btn-primary mymovies-btn mt-3">
+            Cancelar
+          </button>
+        </Link>
       </div>
     </div>
   );
