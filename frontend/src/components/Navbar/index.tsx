a2a860630042ce0fa07191a92ea82939f4443231
Márcio Monks <marcio.monks@gmail.com>
2022-03-07 10:18:54 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import { ReactComponent as GithubIcon } from "assets/img/github.svg";
+import "./styles.css";
+
+function Navbar() {
+  return (
+    <header>
+      <nav className="container">
+        <div className="mymovies-nav-content">
+          <h1>MyMovies</h1>
+          <a href="http://github.com/MarcioMonks">
+            <div className="mymovies-contact-container">
+              <GithubIcon />
+              <p className="mymovies-contact-link">/MarcioMonks</p>
+            </div>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar;
