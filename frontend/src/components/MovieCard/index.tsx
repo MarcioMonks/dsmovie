d171360220416809e882deb67ea0e1723f899b07
Márcio Monks <marcio.monks@gmail.com>
2022-03-11 14:01:10 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -16,7 +16,7 @@ function MovieCard({ movie }: Props) {
       />
       <div className="mymovies-card-bottom-container">
         <h3>{movie.title}</h3>
-        <MovieScore />
+        <MovieScore count={movie.count} score={movie.score} />
 
         <Link to={`/form/${movie.id}`}>
           <div className="btn btn-primary mymovies-btn">Avaliar</div>
